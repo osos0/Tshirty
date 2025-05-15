5054c707f386e20040f7a4fa6184a97841a8345c
Osos0 <osama_magdy0@yahoo.com>
2025-05-15 21:41:07 +0300

    some rename scss fileS

--- src/componants/bigPictures.js
+++ src/componants/bigPictures.js
@@ -3,8 +3,17 @@ import React from "react";
 export default function BigPictures() {
   return (
     <>
-      <div className="container">
-        <div className="row">big</div>
+      <div className="container bigPictuers">
+        <div className="row">
+          <div className="col-lg-6 col-md-6 col-sm-12 son1">
+            <img src="" alt="flashDrive" />
+          </div>
+          <div className="col-lg-6 col-md-6 col-sm-12 son2">2</div>
+          <div className="col-lg-6 col-md-6 col-sm-12 son3">3</div>
+          <div className="col-lg-6 col-md-6 col-sm-12 son4">4</div>
+          <div className="col-lg-6 col-md-6 col-sm-12 son5">5</div>
+          <div className="col-lg-6 col-md-6 col-sm-12 son6">6</div>
+        </div>
       </div>
     </>
   );

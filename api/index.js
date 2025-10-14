3e695c3b211a09f1fa479b461c5daf60d3e3a843
Osos0 <osama_magdy0@yahoo.com>
2025-10-14 21:47:15 +0300

    Starting Backend

--- api/index.js
+++ api/index.js
@@ -1,7 +1,23 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import userRoutes from "./routes/user.routes.js";
+
+dotenv.config({ quiet: true });
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log(" ✅ DB connected");
+  })
+  .catch((err) => {
+    console.error("❌ DB connection error:", err);
+  });
 
 const app = express();
 
-app.listen(3000, () => {
-  console.log("Tshirty server running on http://localhost:3000");
+app.listen(5000, () => {
+  console.log("🚀 Tshirty server running on http://localhost:5000");
 });
+
+app.use("/api/user", userRoutes);

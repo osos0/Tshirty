2ea4ee9eab02a2dafeb504c3b159c78ae218e1d2
Osos0 <osama_magdy0@yahoo.com>
2025-10-16 18:38:05 +0300

    signup Backend

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoutes from "./routes/user.routes.js";
+import authRoutes from "./routes/auth.route.js";
 
 dotenv.config({ quiet: true });
 
@@ -16,8 +17,11 @@ mongoose
 
 const app = express();
 
+app.use(express.json());
+
 app.listen(5000, () => {
   console.log("🚀 Tshirty server running on http://localhost:5000");
 });
 
 app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
